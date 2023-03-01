5c5033cd31cd1e0eb6e76f90b5e541d4a333f276
nisiq <112112965+nisiq@users.noreply.github.com>
2023-03-01 10:43:57 -0300

    Create E1.java

---
+++ E1.java
@@ -0,0 +1,13 @@
+public class p1 {
+    public static void main(String[] args) {
+        int idade = 18;
+        int idade2 = 18;
+
+        double somaidades = idade + idade2;
+
+        System.out.printf("A soma das duas idades é "+somaidades);
+
+
+    }
+}
+=======================================================
