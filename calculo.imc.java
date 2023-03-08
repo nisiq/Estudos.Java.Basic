738df7ad95f2d1cf6dee8f72e399defc988e713f
nisiq <112112965+nisiq@users.noreply.github.com>
2023-03-08 11:28:12 -0300

    Create calculo.imc.java

---
+++ calculo.imc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class D03 {
+    //ler peso e altura do usuário > calcular IMC
+
+    public static void main(String[] args) {
+        System.out.println("Digite o peso do usuário: ");
+    }
+
+
+}
